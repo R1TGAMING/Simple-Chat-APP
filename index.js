da4871ecdbc511572964a3e79fe167816ceea3fd
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-07-29 05:31:34 +0000

    Setup socket

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const { Server } = require("socket.io");
-
-
 const app = express();
+const { Server } = require("socket.io");
+const http = require('http').Server(app);
+const path = require('path');
+
+const io = require("socket.io")(http);
+
 
 
 app.get('/', (req, res) => {
-  
+  res.sendFile(path.join(__dirname + "/templates", 'index.html'))
+})
+
+io.on( 'connection', (socket) => {
+  console.log('a user connected');
+})
+
+http.listen(3000, () => {
+  console.log('server started');
 })
